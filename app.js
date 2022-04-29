a2fe61aac7640c3d774e3e353f9fa64884c032de
Kiefer Weis <kieferweis@protonmail.com>
2022-04-28 21:34:33 -0400

    Fix board generation and add simple takeTurn() method

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 class TicTacToe {
     constructor(size) {
         this.size = size;
-        this.board = new Array(size).fill(new Array(size).fill(' '));
+        this.board = new Array(size);
+        for (let i = 0; i < size; i++) {
+            this.board[i] = new Array(size).fill(' ');
+        }
     }
 
     showBoard() {
@@ -16,6 +19,14 @@ class TicTacToe {
                 this.board[row][col] = char;
             }
         }
+    }
+
+    takeTurn(player) {
+        let position = prompt(`Player ${player}: please enter where you want to put your ${player}. Please enter the row followed by the column, separated by a space.`)
+        let row = +position[0] - 1;
+        let col = +position[2] - 1;
+        this.board[row][col] = player;
+        this.showBoard();
     }
 
 }
