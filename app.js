f25adb0958b2604e4004677bfb0918d0e1e6d674
Kiefer Weis <kieferweis@protonmail.com>
2022-04-28 21:58:57 -0400

    Add method to check for a horizontal/vertical win after every move. Change board size to fixed (3x3)

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 //TIC TAC TOE
 
 class TicTacToe {
-    constructor(size) {
-        this.size = size;
-        this.board = new Array(size);
-        for (let i = 0; i < size; i++) {
-            this.board[i] = new Array(size).fill(' ');
+    constructor() {
+        this.board = new Array(3);
+        for (let i = 0; i < 3; i++) {
+            this.board[i] = new Array(3).fill(' ');
         }
     }
 
@@ -14,8 +13,8 @@ class TicTacToe {
     }
 
     clearBoard(char = ' ') {
-        for (let row = 0; row < this.size; row++) {
-            for (let col = 0; col < this.size; col++) {
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
                 this.board[row][col] = char;
             }
         }
@@ -25,8 +24,28 @@ class TicTacToe {
         let position = prompt(`Player ${player}: please enter where you want to put your ${player}. Please enter the row followed by the column, separated by a space.`)
         let row = +position[0] - 1;
         let col = +position[2] - 1;
-        this.board[row][col] = player;
+
+        if (this.board[row][col] === " ") {
+            this.board[row][col] = player;
+            this.checkForWin(player);
+        } else {
+            console.log("This space is already taken!")
+        }
+        
         this.showBoard();
+    }
+
+    checkForWin(player) {
+        let win = false;
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                if (this.board[row].every(colPos => colPos === player) || this.board.every(rowPos => rowPos[col] === player)) {
+                   win = true; 
+                }
+            }
+        }
+
+        if (win) console.log(`${player} wins!!!`);
     }
 
 }
