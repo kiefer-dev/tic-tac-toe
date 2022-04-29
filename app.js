a5907ee9a2c0eaa89c1e69a02f9a4a4efd006a1e
Kiefer Weis <kieferweis@protonmail.com>
2022-04-28 20:59:52 -0400

    Add clearBoard() method to reset board to given char, or an empty space by default

--- app.js
+++ app.js
@@ -3,13 +3,19 @@
 class TicTacToe {
     constructor(size) {
         this.size = size;
-        this.board = new Array(size).fill(new Array(size).fill(" "));
+        this.board = new Array(size).fill(new Array(size).fill(' '));
     }
 
     showBoard() {
         console.log(this.board);
     }
 
-    
+    clearBoard(char = ' ') {
+        for (let row = 0; row < this.size; row++) {
+            for (let col = 0; col < this.size; col++) {
+                this.board[row][col] = char;
+            }
+        }
+    }
 
 }
